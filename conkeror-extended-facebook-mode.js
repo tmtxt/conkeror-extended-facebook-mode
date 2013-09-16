5608b766ddcc3c813ce06ffec1bda07ad00bf7fa
tmtxt <tmtxt@txt-mac.local>
2013-09-17 00:10:03 +0700

    basic key bindings and function for inspecting story link

--- conkeror-extended-facebook-mode.js
+++ conkeror-extended-facebook-mode.js
@@ -1,1 +1,41 @@
+// include the default facebook mode
+require("facebook");
 
+// facebook shortcut keys
+define_key(facebook_keymap, "j", null, $fallthrough);
+define_key(facebook_keymap, "k", null, $fallthrough);
+define_key(facebook_keymap, "return", null, $fallthrough);
+define_key(facebook_keymap, "q", null, $fallthrough);
+define_key(facebook_keymap, "/", null, $fallthrough);
+define_key(facebook_keymap, "l", null, $fallthrough);
+define_key(facebook_keymap, "m", null, $fallthrough);
+define_key(facebook_keymap, "c", null, $fallthrough);
+
+// function for inspecting and finding the link of selected story
+function facebook_mode_find_story_link(I, open_url_func){
+  var doc = I.buffer.document;
+  var selectedStory = doc.querySelector(".selectedStorySimple");
+  if(selectedStory == null){
+	I.minibuffer.message("No selected story");
+  } else {
+	var timestamp = selectedStory.querySelector(".timestamp");
+	if(timestamp == null){
+	  I.minibuffer.message("Cannot find timestamp link");
+	} else {
+	  var link = timestamp.parentNode;
+	  open_url_func(link, i.window);
+	}
+  }
+}
+
+// interactive command to open selected story
+// new buffer
+interactive("facebook-open-current-story-new-buffer", null, function (I) {
+  facebook_mode_find_story_link(I, load_url_in_new_buffer);
+});
+// new buffer background
+interactive("facebook-open-current-story-new-buffer-background", null, function (I) {
+  facebook_mode_find_story_link(I, load_url_in_new_buffer_background);
+});
+
+provide("conkeror-extended-facebook-mode");
