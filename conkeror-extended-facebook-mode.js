08f91bae69fadffdad69669663eb21d6649e703d
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-05-15 15:50:07 +0700

    fix indent

--- conkeror-extended-facebook-mode.js
+++ conkeror-extended-facebook-mode.js
@@ -556,8 +556,8 @@ interactive("cefm-open-profile",
 
 interactive("cefm-quick-logout",
 			      "Quickly logout from Facebook", function(I){
-					cefm.clickButton(I, cefm.selectors.logoutButton1, cefm.buttonNames.logout);
-					cefm.clickButton(I, cefm.selectors.logoutButton2, cefm.buttonNames.logout);
+                    cefm.clickButton(I, cefm.selectors.logoutButton1, cefm.buttonNames.logout);
+                    cefm.clickButton(I, cefm.selectors.logoutButton2, cefm.buttonNames.logout);
 			      });
 
 interactive("cefm-open-current-story-new-buffer",
